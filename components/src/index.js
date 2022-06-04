f53be27693e50ea4117d8889a1fd670d9d46901e
Plezi Devops <plezidevops@gmail.com>
2022-06-04 05:25:55 -0400

    passing children to components

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CommentDetail from './CommentDetail';
 import { faker } from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 6:00 PM" blogComment="Nice blog post!" />
-      <CommentDetail avatar={faker.image.avatar()} author="Pascal" timeAgo="Today at 11:00 AM" blogComment="You are trippint!" />
-      <CommentDetail avatar={faker.image.avatar()} author="Sokki" timeAgo="Yesterday at 1:00 AM" blogComment="Good for you!" />
+      <ApprovalCard>
+        <div>
+          <h2>Warnning</h2>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 6:00 PM"
+          blogComment="Nice blog post!" />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Pascal" timeAgo="Today at 11:00 AM"
+          blogComment="You are on fire!" />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sokki"
+          timeAgo="Yesterday at 1:00 AM"
+          blogComment="Good for you!" />
+      </ApprovalCard>
     </div>
 
   );
